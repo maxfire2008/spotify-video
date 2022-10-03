a46c3542a07828632d04e5430a014c66fc9f95bf
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-10-03 19:39:32 +1100

    halfway working

--- src/script.js
+++ src/script.js
@@ -27,6 +27,21 @@ async function youtube_search(query) {
     return await result.text();
 }
 
+async function update() {
+    var currently_playing = await get_currently_playing();
+    console.log(currently_playing);
+    var metadata_string = spotify_to_metadata_string(currently_playing);
+    var target_video_id = await youtube_search(metadata_string);
+
+    var video_player = document.getElementById("video_player");
+
+    if (!video_player.src.includes(target_video_id)) {
+        video_player.src = "https://www.youtube.com/embed/"+target_video_id+"?start=70"
+    }
+
+    // https://www.youtube.com/embed/lrzKT-dFUjE?start=70
+}
+
 async function open_video() {
     var currently_playing = await get_currently_playing();
     console.log(currently_playing);
